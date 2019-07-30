cb0377c9887949ef8f16629d64d44996fb76d3ee
laodingdang <13269735167@163.com>
2019-07-30 12:05:24 +0800

    first add

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="menu">
+    <div class="h1">开始游戏</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.menu {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .h1 {
+    font-size: 56px;
+    text-align: center;
+    height: 80px;
+    padding: 24px;
+  }
+  .h1:hover {
+    color: #fff;
+    background: rgba(0, 0, 0, 0.8)
+  }
+}
+</style>
